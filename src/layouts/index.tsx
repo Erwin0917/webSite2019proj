7218b1b0d08fc072e6b54a3b61614f99261d2ed2
Erwin Heba <e.heba@getprintbox.com>
2019-11-05 21:24:57 +0100

    fix global styles path in layout file

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Helmet from 'react-helmet';
 
-import { GlobalStyles } from '../styles/general/globalStyles';
+import { GlobalStyles } from '../styles/general/GlobalStyles';
 
 interface PageWrapperProps extends React.HTMLProps<HTMLDivElement> {
 	children: any;
